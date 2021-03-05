645102cfe0df9abed618f888678e356777285770
Gabriel Rezende <g@brielrezen.de>
2021-03-05 10:57:08 -0300

    exibindo notas com css atualizado

--- controllers/NotasController.js
+++ controllers/NotasController.js
@@ -5,7 +5,9 @@ const NotasController = {
   index: async (req, res) => {
     const db = new Sequelize(config)
     // let idNota = 1
-    const result = await db.query('select * from notas' , // 'select * from notas WHERE nota_id = :idNota'
+    const fixos = await db.query('select * from notas WHERE pin = 1;', {type: Sequelize.QueryTypes.SELECT});
+    const naoFixos = await db.query('select * from notas WHERE pin = 0;', {type: Sequelize.QueryTypes.SELECT});
+    const result = await db.query('select * from notas;' , // 'select * from notas WHERE nota_id = :idNota'
     {
       // replacements:{
       //   idNota
@@ -13,7 +15,7 @@ const NotasController = {
       type: Sequelize.QueryTypes.SELECT}); 
     console.log(result);
     console.log(result.length);
-    res.render('index', { result })
+    res.render('index', { result, fixos, naoFixos })
   },
   notFound: (req, res) => {
     res.status(404).render('404', { pagina: req.url})
