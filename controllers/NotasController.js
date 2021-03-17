31466f1cea42d080a5691f72456d9f600603a2c9
Gabriel Rezende <g@brielrezen.de>
2021-03-17 16:28:27 -0300

    limpeza no código

--- controllers/NotasController.js
+++ controllers/NotasController.js
@@ -4,7 +4,6 @@ const config = require('../config/database')
 const NotasController = {
   index: async (req, res) => {
     const db = new Sequelize(config)
-    // let idNota = 1
     const fixos = await db.query('select * from notas WHERE pin = 1;', {type: Sequelize.QueryTypes.SELECT});
     const naoFixos = await db.query('select * from notas WHERE pin = 0;', {type: Sequelize.QueryTypes.SELECT});
     const result = await db.query('select * from notas;' , // 'select * from notas WHERE nota_id = :idNota'
@@ -13,8 +12,7 @@ const NotasController = {
       //   idNota
       // },
       type: Sequelize.QueryTypes.SELECT}); 
-    console.log(result);
-    console.log(result.length);
+
     res.render('index', { result, fixos, naoFixos })
   },
   notFound: (req, res) => {
