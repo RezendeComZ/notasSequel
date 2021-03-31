71308af3b4881a615bd0aaf2d0721bbe62467eb1
Gabriel Rezende <g@brielrezen.de>
2021-03-30 21:29:24 -0300

    rota mudada para /notas

--- controllers/NotasController.js
+++ controllers/NotasController.js
@@ -2,7 +2,6 @@ const { Nota } = require('../models') // O index vai retonar cada atribudo, por 
 
 const NotasController = {
   index: async (req, res) => {
-    // Vai valer mesmo a partir daqui em breve:
     const todas = await Nota.findAll() // db.query('select * from notas;' , // 'select * from notas WHERE nota_id = :idNota'
     const fixos = await Nota.findAll({
       where: {
@@ -15,7 +14,7 @@ const NotasController = {
       }
     });
     // console.log(naoFixos)
-    res.render('index', { todas, fixos, naoFixos })
+    res.render('notas', { todas, fixos, naoFixos })
   },
   notFound: (req, res) => {
     res.status(404).render('404', { pagina: req.url})
