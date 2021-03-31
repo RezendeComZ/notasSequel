2eba8ef722021a49089e50446b6fc01644bf4d07
Gabriel Rezende <g@brielrezen.de>
2021-03-30 21:36:04 -0300

    novo index com redirect

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 const NotasController = require('../controllers/NotasController')
 
+/* Login / Redirecionamento */
+router.get('/', (req, res) => {
+  res.render('index')
+});
+
 /* Lista de notas */
-router.get('/', NotasController.index);
+router.get('/notas', NotasController.index);
 
 /* 404 */
 router.use(NotasController.notFound)
