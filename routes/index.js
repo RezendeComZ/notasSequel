9eafffdd90709c5a74282f5d8008859ee9decd05
Gabriel Rezende <g@brielrezen.de>
2021-04-01 12:36:15 -0300

    setup inicial do express validator

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const NotasController = require('../controllers/NotasController')
+const { check, validationResult, body } = require ('express-validator')
 
 /* Home / Redirecionamento */
 router.get('/', (req, res) => {
@@ -10,6 +11,15 @@ router.get('/', (req, res) => {
 router.get('/login', (req, res) => {
   res.render('login')
 });
+router.post('/login', [
+  check("email").isEmail(),
+  check("password").isLength({min: 6, max: 150})
+], (req, res) => {
+  let { email, password} = req.body
+  console.log('Requisição post: ' + email + ' senha: ' + password)
+  console.log(validationResult(req))
+  res.redirect('notas')
+})
 
 /* Lista de notas */
 router.get('/notas', NotasController.index);
