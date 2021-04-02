2639db9d1209bcc02a2b870a0e62c37a8f78e9ac
Gabriel Rezende <g@brielrezen.de>
2021-04-02 20:16:54 -0300

    preparando para usuários dinâmicos

--- controllers/NotasController.js
+++ controllers/NotasController.js
@@ -1,11 +1,18 @@
-const { Nota } = require('../models') // O index vai retonar cada atribudo, por isso desestruturar
+const { Nota, Usuario } = require('../models') // O index vai retonar cada atribudo, por isso desestruturar
 
 const NotasController = {
   index: async (req, res) => {
-    const todas = await Nota.findAll() // db.query('select * from notas;' , // 'select * from notas WHERE nota_id = :idNota'
+    const todas = await Nota.findAll({
+      include: { // incluindo a FK
+        model: Usuario,
+        as: 'user',
+        required: true
+      }
+    }) // db.query('select * from notas;' , // 'select * from notas WHERE nota_id = :idNota'
     const fixos = await Nota.findAll({
       where: {
-        pin: true
+        pin: true,
+        user_id: 1 // Deixar isso dinâmico futuramente // assim como o naoFixos
       }
     });
     const naoFixos = await Nota.findAll({
@@ -13,7 +20,7 @@ const NotasController = {
         pin: false
       }
     });
-    console.log(todas)
+    console.log(todas[0].user.user_name)
     res.render('notas', { todas, fixos, naoFixos })
   },
   notFound: (req, res) => {
