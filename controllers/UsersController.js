1eb0504a00f38043de2d4e2edc7da2486639c15b
Gabriel Rezende <g@brielrezen.de>
2021-04-02 21:01:51 -0300

    funcao de autenticacao melhorada. Pagina de criar criada

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,14 +3,21 @@ const bcrypt =  require('bcrypt')
 const { check, validationResult, body } = require ('express-validator')
 
 const UsersController = {
+  // Login
   loginGet: (req, res) => {
     res.render('login')
   },
   loginPost:(req, res) => {
-    let { email, password} = req.body
-    console.log('Requisição post: ' + email + ' senha: ' + password)
+    let { email } = req.body
+    req.body.password = bcrypt.hashSync(req.body.password, 10)
+    console.log('Requisição post: ' + email + ' senha: ' + req.body.password)
     console.log(validationResult(req))
-    res.redirect('notas')
+    res.send('página de autenticacao')
+    // res.redirect('notas')
+  },
+  // Criar
+  registroGet(req, res) { // usando outro tipo de função só para variar
+    res.send('Página de registro')
   }
 }
 
