974b31255ea34b96a37697d76e71b7e7bc3c64d3
Gabriel Rezende <g@brielrezen.de>
2021-04-03 11:46:41 -0300

    login de verificação funcional

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -7,13 +7,23 @@ const UsersController = {
   loginGet: (req, res) => {
     res.render('login')
   },
-  loginPost:(req, res) => {
+  loginPost: async (req, res) => {
     let { email } = req.body
-    req.body.password = bcrypt.hashSync(req.body.password, 10)
-    console.log('Requisição post: ' + email + ' senha: ' + req.body.password)
-    console.log(validationResult(req))
-    res.send('página de autenticacao')
-    // res.redirect('notas')
+    const userDb = await Usuario.findOne({ where: { email }  })
+    // console.log('dados do userdb: ' + userDb[0].email + ' pass da DB: ' + userDb[0].user_pass)
+    // req.body.password = bcrypt.hashSync(req.body.password, 10)
+    // console.log('Requisição post: ' + email + ' senha: ' + req.body.password)
+    // console.log('Express-validator: ', validationResult(req))
+    let validade = false;
+    if (userDb) {
+      console.log('entrou no if')
+      validade = bcrypt.compareSync(req.body.password, userDb.user_pass)
+    }
+    if (validade) {
+     res.redirect('notas')
+    } else {
+      res.send('não passou')
+    }
   },
   // Criar
   registroGet(req, res) { // usando outro tipo de função só para variar
