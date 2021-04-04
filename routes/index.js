7dcacb3ab3b8e31426eb901842bafc598b62b5aa
Gabriel Rezende <g@brielrezen.de>
2021-04-04 12:10:50 -0300

    validação de registro

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,17 @@ router.post('/autenticar',
 /* Registro */
 router.get('/registro', UsersController.registroGet)
 
+router.post('/registrando', 
+  body("email").isEmail().bail(),
+  body('text')
+    .not()
+    .isEmpty().withMessage('O campo de nome não pode estar vazio')
+    .trim().escape()
+    .isLength({min: 2, max: 40}).withMessage('Você precisa preencher ao menos 2 caracteres')
+    .bail(),
+  body("password").isLength({min: 6, max: 150}).bail()
+, UsersController.registroPost)
+
 /* Logout */
 router.get('/logout', UsersController.logout);
 
