6ac7681fedec69737c1fbb8899a7fd4408215bf2
Gabriel Rezende <g@brielrezen.de>
2021-04-04 18:06:54 -0300

    código js do front parcialmente refatorado

--- public/javascripts/front.js
+++ public/javascripts/front.js
@@ -1,43 +1,45 @@
+const title = document.getElementById('title')
+const body = document.getElementById('body')
+
 const btApagar = (id) => {
     document.getElementById(id).style.display = 'none'  
 }
 
 // Janela inserir:
 
-// document.onkeyup = ev => { // Deprecated
-//     if (ev.keyCode === 192) mostraInserir();
-//   }
 document.onkeyup = ev => {
     if (ev.key == 'Dead' && ev.code == 'Backquote') mostraInserir();
   }
   
-  const mostraInserir = () => {
-    document.querySelector('.inserir').style.display = 'block';
-    document.getElementById("headField").focus()
-  }
+const mostraInserir = () => {
+  document.querySelector('.inserir').style.display = 'block';
+  document.getElementById("title").focus()
+}
   
-  const escondeInserir = () => {
-    document.querySelector('.inserir').style.display = 'none'
-  }
+const escondeInserir = () => {
+  document.querySelector('.inserir').style.display = 'none'
+}
 
-  const deuEnter = (event) => {
-    if (event.keyCode === 13) btEnviar();
-  }
-  const deuCtrlEnter = (event) => {
-    if (event.keyCode === 13 && event.ctrlKey) {
-        document.forms[0].submit();;
-        btEnviar();
-    };
-    if (event.keyCode === 13 && event.metaKey) {
-        document.forms[0].submit();;
-        btEnviar()
-    }; /// via CMD
-  }
+const deuEnter = (event) => {
+  if (event.keyCode === 13) btEnviar();
+}
+const deuCtrlEnter = (event) => {
+  if (event.keyCode === 13 && event.ctrlKey) {
+      document.forms[0].submit();;
+      btEnviar();
+  };
+  if (event.keyCode === 13 && event.metaKey) {
+      document.forms[0].submit();;
+      btEnviar()
+  }; /// via CMD
+}
 
-const btEnviar = () => {
-    if (headField.value === '') {
-      alert('Preencha ao menos o título')
+const btEnviar = document.getElementById('btEnviar')
+btEnviar.addEventListener('click', (ev) => {
+    if (!title.value && !body.value) {
+      alert('Preencha ao menos um campo')
+      ev.preventDefault();
     } else {
       escondeInserir()
     }
-  }
+  })
